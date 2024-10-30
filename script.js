fc53812520cf8fd8ff79efd761abb4f3ce61d5e8
Tamanna12308 <tams240330@gmail.com>
2024-10-30 11:49:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 // A basic script to log a message in the console
 console.log("Welcome to my portfolio site!");
+// Particle effect on mouse movement with trail
+document.addEventListener('mousemove', (e) => {
+    createParticle(e.pageX, e.pageY);
+});
 
+// Handles scrolling as well as mouse movement
+document.addEventListener('scroll', (e) => {
+    createParticle(event.clientX, event.clientY);
+});
+
+function createParticle(x, y) {
+    const particle = document.createElement('div');
+    particle.classList.add('particle');
+
+    const size = Math.random() * 10 + 5; // Random size between 5 and 15
+    particle.style.width = `${size}px`;
+    particle.style.height = `${size}px`;
+
+    particle.style.left = `${x}px`; // Corrected particle positioning
+    particle.style.top = `${y}px`;
+
+    document.body.appendChild(particle);
+
+    // Fade out and remove particle after animation
+    particle.addEventListener('animationend', () => {
+        particle.remove();
+    });
+}
 
